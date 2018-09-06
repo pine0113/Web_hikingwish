6c08beeadc9b94a9c20e905b048637a273c07b27
Duncan <loxa10077@gmail.com>
2018-09-06 10:01:46 +0800

    add user name to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # email 跟 password欄位 deivse 已強制要求
+  validates_presence_of :name
+
 end
