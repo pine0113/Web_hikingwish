63b6bf1129b06e866c771bfaf307386757d833dc
Duncan <loxa10077@gmail.com>
2018-09-06 22:55:31 +0800

    lazy test avatar upload and bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,22 @@ class UsersController < ApplicationController
 
 
   def index
-    
+    @users = User.all
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:name, :intro, :avatar))
+      flash[:notice] = "個人資料更新成功"
+      redirect_to users_path
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
 end
