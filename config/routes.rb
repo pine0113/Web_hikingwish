83b784f9051fa0d675b1da2989202bc6f80d3cb3
sinash <pine0113@gmail.com>
2018-09-10 10:00:30 +0800

    add basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
+  get 'pages/about', :to => 'pages#about'
+  
   resources :users, only: [:index, :show, :edit, :update]
   root "users#index"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Users
+User.destroy_all
+  User.create(name:"pine0113",email: "pine0113@gmail.com", password: "12345678", role: "admin")
+  User.create(name:"admin",email: "admin@example.com", password: "12345678", role: "admin")
+  User.create(name:"user",email: "user@example.com", password: "12345678", role: "user")
+  
+puts "Default users created!"
