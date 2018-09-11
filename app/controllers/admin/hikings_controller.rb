c00fdafc696e7a61442f977dc0b28ff17e1fa006
Duncan <loxa10077@gmail.com>
2018-09-11 10:23:33 +0800

    admin can create hiking

--- app/controllers/admin/hikings_controller.rb
+++ app/controllers/admin/hikings_controller.rb
@@ -1,2 +1,25 @@
 class Admin::HikingsController < ApplicationController
+
+  def new
+    @hiking = Hiking.new
+  end
+
+  def create
+    @hiking = Hiking.new(hiking_params)
+
+    if @hiking.save
+      flash[:notice] = "已成功新增路線"
+      redirect_to admin_plans_path
+    else
+      flash[:alert] = @hiking.errors.full_messages.to_centence
+      render :new
+    end
+  end
+
+  private
+
+  def hiking_params
+    params.require(:hiking).permit(:name, :iamge, :level, :day, :miles, :intro)
+  end
+
 end
--- app/models/hiking.rb
+++ app/models/hiking.rb
@@ -1,2 +1,3 @@
 class Hiking < ApplicationRecord
+  validates_presence_of :name
 end
