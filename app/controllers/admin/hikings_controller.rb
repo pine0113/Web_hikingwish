2e9cba532758b8f4ed924b1f3ecd4cc28012a207
Duncan <loxa10077@gmail.com>
2018-09-11 10:52:34 +0800

    add hiking destroy action

--- app/controllers/admin/hikings_controller.rb
+++ app/controllers/admin/hikings_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HikingsController < ApplicationController
-  before_action :find_hiking, only: [:edit, :update]
+  before_action :find_hiking, only: [:edit, :update, :destroy]
 
   def index
     @hikings = Hiking.all
@@ -32,6 +32,12 @@ class Admin::HikingsController < ApplicationController
       flash[:alert] = @hiking.errors.full_messages.to_sentence
       render :edit
     end
+  end
+
+  def destroy
+    @hiking.destroy
+    flash[:alert] = "路線已刪除"
+    redirect_to admin_hikings_path
   end
 
   private
