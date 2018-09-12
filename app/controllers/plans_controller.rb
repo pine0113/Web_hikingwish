ed8cebcbd39417e529f2669580c47e541757604b
Duncan <loxa10077@gmail.com>
2018-09-12 21:17:44 +0800

    user can create plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,6 +4,22 @@ class PlansController < ApplicationController
     @plans = Plan.all
   end
 
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.user = current_user
+    if @plan.save
+      flash[:notice] = "計畫建立完成"
+      redirect_to plans_path
+    else
+      flash[:alert] = @plan.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
   def show
   end
 
@@ -14,6 +30,12 @@ class PlansController < ApplicationController
   end
 
   def search
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:name, :image, :start_date, :end_date, :budget_top, :budget_bottom, :budget_final, :level, :day, :miles, :intro)
   end
 
 end
