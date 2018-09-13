bcf8d02db6c7ed9181abd4a883c00c894c5953bc
pinkg023 <pinkg023@g.ncu.edu.tw>
2018-09-13 12:23:13 +0800

    wish delete function

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,5 +1,5 @@
 class WishesController < ApplicationController
-  before_action :set_wish, :only => [:edit, :update]
+  before_action :set_wish, :only => [:edit, :update, :destroy]
 
   def index
     @wishes = current_user.wishes.order(created_at: :desc)
@@ -26,6 +26,11 @@ class WishesController < ApplicationController
     else
       flash[:alert] = "wish was failed to create"
     end
+    redirect_to wishes_path
+  end
+
+  def destroy
+    @wish.destroy
     redirect_to wishes_path
   end
 
