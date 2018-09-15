c24362f2419b7ff3f9df7ee4dcf9cc3f22a73fb9
sinash <pine0113@gmail.com>
2018-09-15 15:20:47 +0800

    fix plan owner_id

--- app/models/hiking.rb
+++ app/models/hiking.rb
@@ -1,7 +1,8 @@
 class Hiking < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, ImageUploader
-
+  acts_as_taggable_on :tags
+  
   has_many :plans
   has_many :wishes
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,6 +2,7 @@ class Plan < ApplicationRecord
   #mount_uploader :image, ImageUploader
   belongs_to :user , :class_name => "User", :foreign_key => "owner_id"
   has_many :notification, :as => :notifiable
+
   validates :name, :presence => true
-  validates :user_id, :presence => true # 一定要有user id
+  validates :owner_id, :presence => true # 一定要有user id
 end
